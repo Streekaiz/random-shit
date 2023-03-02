90385a73404c5e5b44cdb82159fff6df62d10d77
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2023-03-01 17:47:48 -0800

    Update gamesneese.lua

--- gamesneese.lua
+++ gamesneese.lua
@@ -1308,7 +1308,7 @@ do
                 local preview_heatlhbar = utility:Create("Frame", {Vector2.new(1, 1), preview_heatlhbaroutline}, {
                     Size = utility:Size(1, -2, 1, -2, preview_heatlhbaroutline),
                     Position = utility:Position(0, 1, 0, 1, preview_heatlhbaroutline),
-                    Color = Color3.fromRGB(255, 0, 0),
+                    Color = Color3.fromRGB(0, 255, 0),
                     Filled = true
                 }, window.VisualPreview.Drawings);healthbar = preview_heatlhbar
                 --
